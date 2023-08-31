0c27b0dc4df89e209f9dffb343386dd5925685d6
Andrey Krushelnitsky <akrush24@gmail.com>
2023-08-31 20:52:40 +0300

    add uwsgi support

--- venv.sh
+++ venv.sh
@@ -3,7 +3,8 @@ set -ex
 python3 -m venv ./venv
 source ./venv/bin/activate
 if [[ ${1} == '-i' ]]; then
+  shift
   python3 -m pip install --upgrade pip
   pip3 install -r ./requirements.txt
 fi
-./run.me
+./run.me ${1}
